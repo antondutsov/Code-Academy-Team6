ed0c983915d487ee9d2029c41937d5e198067476
antondutsov <anton@dutsov.com>
2021-01-22 14:32:53 +0200

    updated project

--- Creating htoi function/main.c
+++ Creating htoi function/main.c
@@ -1,56 +1,45 @@
-#include <stdio.h>
-// #include <string.h>
+/*
+Напишете функция htoi(s), която преобразува низ от шестнадесетични цифри в
+еквивалентна целочислена стойност. Възможните цифри са между 0 и 9,
+от а до f и от А до F.
+*/
 
-int htoi (char *[]);
+#include <stdio.h>
+
+unsigned long htoi (char s[]);
 
 int main (){
     char s[10];
     printf ("0x");
-
     scanf("%s", s);
-
-    int rez=htoi(s);
-    printf ("%s\n",s);
-    printf ("%d\n",rez);
-
-return 0;
+    unsigned long rez=htoi(s);
+    printf ("Hex: %s\n",s);
+    printf ("Dec: %ld\n",rez);
+    return 0;
 }
-
-int htoi (char *s[]){
+unsigned long htoi (char s[]){
     int i=0;
     int br=0;
-    int sm=0;
-    int sum=0;
-
-    for (br=0;s[br]!=0;br++); // br = strlen(s);
-
+    int number=0;
+    unsigned long sum=0;
+    for (br=0;s[br]!=0;br++);
     for (i=0;i<br;i++){
         if (s[i]>='0' && s[i]<='9') {
-        printf("first if: %d \n", i);
-        sm = s[i] - 'e0';
-        printf("'first sm': %d \n", sm);
+            number = s[i] - '0';
         }
-
         else if (s[i]>='A' && s[i]<='F'){
-        printf("first elseif: %d \n", i);
-        sm = s[i] - 'A';
-        printf("'second sm': %d \n", sm);
-        sm+=10;
+            number = s[i] - 'A';
+            number+=10;
         }
-
         else if (s[i]>='a' && s[i]<='f'){
-        printf("second elseif: %d \n", i);
-        sm = s[i] - 'a';
-        printf("'third sm': %d \n", sm);
-        printf("%d, %d, %c",sm = (s[i] - 'a'));
-        sm+=10;
+            number = s[i] - 'a';
+            number+=10;
         }
-
         else{
             printf ("Invalid input\n");
+            break;
         }
-        sum = sum*16 + (sm);
-        printf("sum: %d \n", sum);
+        sum = sum*16 + (number);
     }
-return sum;
+    return sum;
 }
